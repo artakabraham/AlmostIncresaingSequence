0505239fd8c888e06560d11fe78ebbc62b4c255f
Artak <abrahamyan@outlook.com>
2021-05-20 14:37:38 +0400

    Update Program.cs

--- AlmostIncresaingSequence/Program.cs
+++ AlmostIncresaingSequence/Program.cs
@@ -4,52 +4,108 @@ namespace AlmostIncresaingSequence
 {
     class Program
     {
+        /// <summary>
+        /// Given a sequence of integers as an array, determine whether it is possible to obtain a strictly increasing sequence by removing no more than one element from the array.
+        /// Note: sequence a0, a1, ..., an is considered to be a strictly increasing if a0<a1< ... < an.
+        /// Sequence containing only one element is also considered to be strictly increasing.
+        /// </summary>
         static void Main(string[] args)
         {
-
-            int[] array = new int[] { 1, 2, 1, 2 };
-
+            //int[] array = new int[] { 1, 2, 1, 2 };
+            //int[] array = new int[] { 1, 3, 2, 1 };
+            //int[] array = new int[] { 1, 3, 2, 3 };
+            //int[] array = new int[] { 10, 1, 2, 3, 4, 5 };
+            //int[] array = new int[] { 5, 6, 7, 8, 9, 10, 1 };
+            //int[] array = new int[] { 40, 50, 60, 10, 20, 30 };
+            //int[] array = new int[] { 40, 50, 60, 70 };
+            //int[] array = new int[] { 1, 1, 2, 3, 4, 4 };
+            int[] array = new int[] { 1, 2, 5, 3, 5 };
             var answ = IsInereasing(array);
-
             Console.WriteLine(answ);
-
         }
 
-        static bool IsInereasing(int[] array)
+        static bool IsInereasing(int[] sequence)
         {
-
-            int temp;
-            for (int i = 0; i < array.Length; i++)
-            {
-                for (int j = i + 1; j < array.Length; j++)
-                {
-                    if (array[j] < array[i])
-                    {
-                        temp = array[j];
-                        array[j] = array[i];
-                        array[i] = temp;
-                    }
-                }
-            }
-
-
-            if (array.Length > 0 && array.Length < 2)
+            if (sequence.Length > 0 && sequence.Length < 2)
             {
                 return true;
             }
-            int t = 0;
-            for (int i = 0; i < array.Length - 1; i++)
+            int temp = 0;
+            int j = 0;
+            for (int i = 0; i < sequence.Length - 1; i++)
             {
-                if (array[i] >= array[i + 1])
+                if (sequence[i] >= sequence[i + 1])
                 {
-                    t++;
+                    j = i;
+                    temp++;
                 }
-                if (t > 1)
+                if (temp > 1)
                 {
                     return false;
                 }
             }
-            return true;
+            if (j == 0 || j == sequence.Length - 1)
+            {
+                return true;
+            }
+
+            int[] arr1(int[] array)
+            {
+                int[] newSeq = new int[array.Length - 1];
+                for (int i = 0; i < j; i++)
+                {
+                    newSeq[i] = array[i];
+                }
+                for (int i = j; i < array.Length - 1; i++)
+                {
+                    newSeq[i] = array[i + 1];
+                }
+                return newSeq;
+            }
+
+            int[] arr2(int[] array)
+            {
+                int[] newSeq = new int[array.Length - 1];
+                j++;
+                for (int i = 0; i < j; i++)
+                {
+                    newSeq[i] = array[i];
+                }
+                for (int i = j; i < array.Length - 1; i++)
+                {
+                    newSeq[i] = array[i + 1];
+                }
+                return newSeq;
+            }
+
+            bool IsInc(int[] array)
+            {
+                int k = 0;
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (array[i] >= array[i + 1])
+                    {
+                        k++;
+                    }
+                }
+                if (k > 0)
+                    return false;
+                return true;
+            }
+            bool c1 = IsInc(arr1(sequence));
+            if (c1)
+            {
+                return true;
+            }
+            bool c2 = IsInc(arr2(sequence));
+            if (c2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
